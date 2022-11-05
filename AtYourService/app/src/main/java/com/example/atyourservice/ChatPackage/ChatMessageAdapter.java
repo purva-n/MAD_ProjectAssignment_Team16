8ffb6963ff0942a96cc67f6d716ab233ec3b8eed
divyajangid2496 <92000178+divyajangid2496@users.noreply.github.com>
2022-11-04 22:50:39 -0700

    merging master changes

---
+++ AtYourService/app/src/main/java/com/example/atyourservice/ChatPackage/ChatMessageAdapter.java
@@ -0,0 +1,35 @@
+package com.example.atyourservice.ChatPackage;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.atyourservice.ReceivedMessagesHolder;
+import com.example.atyourservice.models.Message;
+
+import java.util.List;
+
+public class ChatMessageAdapter extends RecyclerView.Adapter<ReceivedMessagesHolder> {
+    //create class of messages to hold ,
+    public ChatMessageAdapter(List<Message>, Context context){
+
+    }
+    @NonNull
+    @Override
+    public ReceivedMessagesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReceivedMessagesHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+*/
