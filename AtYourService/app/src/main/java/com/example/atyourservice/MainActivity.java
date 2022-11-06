58ec804f96c3271fff7b154e5fb07c6381e9c3a1
Debra Xu <wex007@ucsd.edu>
2022-11-05 20:47:41 -0700

    2nd ver

--- AtYourService/app/src/main/java/com/example/atyourservice/MainActivity.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/MainActivity.java
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
+=======
+//package com.example.atyourservice;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Button toGatherButton = findViewById(R.id.toGather);
+//        toGatherButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // need to add chat lists activity below
+//                // Intent intent1 = new Intent(this,{Insert Chat List activity});
+//                //startActivity(intent1);
+//            }
+//        });
+//    }
+//
+//    public void onClick(View view) {
+//        Intent intent = new Intent(this, EventSearchActivity.class);
+//        startActivity(intent);
+//    }
+//
+//}
+
+>>>>>>> 5df33ab (add about me and user profile image)
 package com.example.atyourservice;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,14 +43,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+<<<<<<< HEAD
 import com.example.atyourservice.ChatPackage.MessagesActivity;
 
 public class MainActivity extends AppCompatActivity {
+=======
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnAYService,btnFirebase,btnToGather;
+>>>>>>> 5df33ab (add about me and user profile image)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
         Button firebaseButton = findViewById(R.id.fireBaseAssignment);
         firebaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,3 +73,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+=======
+
+        btnAYService = (Button) findViewById(R.id.Week7Assignment);
+        btnAYService.setOnClickListener(this);
+        btnFirebase = (Button) findViewById(R.id.fireBaseAssignment);
+        btnFirebase.setOnClickListener(this);
+        btnToGather = (Button) findViewById(R.id.toGather);
+        btnToGather.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.Week7Assignment:
+                openAtYourServiceActivity();
+                break;
+            case R.id.fireBaseAssignment:
+                openFirebaseActivity();
+                break;
+            case R.id.toGather:
+                openToGatherActivity();
+                break;
+        }
+    }
+
+    private void openToGatherActivity() {
+
+    }
+
+    public void openAtYourServiceActivity(){
+        Intent intent = new Intent(this,EventSearchActivity.class);
+        startActivity(intent);
+    }
+    public void openFirebaseActivity(){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
+}
+
+>>>>>>> 5df33ab (add about me and user profile image)
---
+++ AtYourService/app/src/main/java/com/example/atyourservice/RegisterActivity.java
@@ -0,0 +1,120 @@
+package com.example.atyourservice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.RenderProcessGoneDetail;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.atyourservice.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://mad-assignment-8-default-rtdb.firebaseio.com/");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText userNameInput = (EditText) findViewById(R.id.userName);
+        final Button register = findViewById(R.id.btnReg);
+        final Button login = findViewById(R.id.btnLogin);
+        final Button about = findViewById(R.id.btnAbout);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String logUser = userNameInput.getText().toString();
+//                final String dummy = logUser+"123";
+                final String password = "123456";
+                startActivity(new Intent(RegisterActivity.this,UserList.class));
+//                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                if (logUser == null) {
+                    userNameInput.setError("Please enter username!");
+                }else{
+                    databaseReference.child("senders").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(logUser)){
+                                final String getPassword = snapshot.child(logUser).child("Password").getValue(String.class);
+                                if (getPassword.equals(password)){
+                                    Toast.makeText(RegisterActivity.this,"Logged in successfully!",Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }else{
+                                    Toast.makeText(RegisterActivity.this,"Logged in unsuccessfully!",Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(RegisterActivity.this,"User registered successfully",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String regUser = userNameInput.getText().toString();
+//                final String dummy = regUser+"123";
+                final String password = "123456";
+//                final String uriImage = "android.resource://com.example.atyourservice/drawable/ic_";
+                if (regUser == null) {
+                    userNameInput.setError("Please Enter Username!");
+                }else{
+                    databaseReference.child("senders").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(regUser)){
+                                Toast.makeText(RegisterActivity.this,"The username is already registered",Toast.LENGTH_SHORT).show();
+                            }else{
+                                databaseReference.child("senders").child(regUser).child("Username").setValue(regUser);
+                                databaseReference.child("senders").child(regUser).child("Password").setValue(password);
+//                                databaseReference.child("senders").child(regUser).child("UriImage").setValue(uriImage);
+                                Toast.makeText(RegisterActivity.this,"User registered successfully",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
