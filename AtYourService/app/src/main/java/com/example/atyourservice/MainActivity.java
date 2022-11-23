6ef64ecb07a36d4be7774ddd76dbb7415ab0c409
divyajangid2496 <92000178+divyajangid2496@users.noreply.github.com>
2022-11-22 22:56:49 -0800

    Action bar implementation

--- AtYourService/app/src/main/java/com/example/atyourservice/MainActivity.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.atyourservice.togather.HomePageActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+
+        Button toGather = findViewById(R.id.toGather);
+
+        toGather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchToGather();
+            }
+        });
+    }
+
+    private void launchToGather() {
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
     }
 
     public void onClick(View view) {
