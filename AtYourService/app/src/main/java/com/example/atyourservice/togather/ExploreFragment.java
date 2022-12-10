455a89230373902155e236b2017def72c2414981
divyajangid2496 <92000178+divyajangid2496@users.noreply.github.com>
2022-12-10 14:12:09 -0800

    Location changes

--- AtYourService/app/src/main/java/com/example/atyourservice/togather/ExploreFragment.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/togather/ExploreFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,18 +15,30 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.atyourservice.R;
 import com.example.atyourservice.api.response.pojo.Groups;
 import com.example.atyourservice.api.response.pojo.Messages;
 import com.example.atyourservice.models.Group;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,16 +46,10 @@ import com.google.firebase.database.ValueEventListener;
  * create an instance of this fragment.
  */
 public class ExploreFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText location;
+    private Button searchGroup;
     private DatabaseReference dbRef;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -52,17 +59,11 @@ public class ExploreFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ExploreFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ExploreFragment newInstance(String param1, String param2) {
+    public static ExploreFragment newInstance() {
         ExploreFragment fragment = new ExploreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -70,8 +71,6 @@ public class ExploreFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -85,7 +84,43 @@ public class ExploreFragment extends Fragment {
         setSpinnerConfiguration(li);
         findGroups(li);
 
+        location = li.findViewById(R.id.locationSearch);
+
+        Places.initialize(((AppCompatActivity) getActivity()).getApplicationContext(), "AIzaSyCeUHVf4a46VpARdx0mTUiQBEE13BnMNws");
+
+        location.setFocusable(false);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(getActivity());
+
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        searchGroup = li.findViewById(R.id.findGroups);
+
+        searchGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("LOCATION SELECTED IS :: "+location.getText());
+            }
+        });
         return li;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == AutocompleteActivity.RESULT_OK) {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            location.setText(place.getAddress());
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR ) {
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Toast.makeText(((AppCompatActivity) getActivity()).getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void findGroups(View view) {
--- AtYourService/app/src/main/java/com/example/atyourservice/togather/GroupInfoPage.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/togather/GroupInfoPage.java
@@ -79,4 +79,5 @@ public class GroupInfoPage extends Fragment {
         return view;
 
     }
+
 }
