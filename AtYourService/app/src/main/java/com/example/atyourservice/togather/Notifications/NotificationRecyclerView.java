a52c3dcbc799d2fe7c65124b82200739d6e10176
Manu Singh <manbir410@gmail.com>
2022-12-10 23:00:10 -0800

    OnClick Listener for Notifications

--- AtYourService/app/src/main/java/com/example/atyourservice/togather/Notifications/NotificationRecyclerView.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/togather/Notifications/NotificationRecyclerView.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,7 +15,7 @@ import com.example.atyourservice.api.response.pojo.Notifications;
 import com.example.atyourservice.models.Notification;
 import com.google.firebase.database.DatabaseReference;
 
-public class NotificationRecyclerView  extends Fragment {
+public class NotificationRecyclerView  extends Fragment implements SelectListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private DatabaseReference dbRef;
@@ -51,10 +52,13 @@ public class NotificationRecyclerView  extends Fragment {
         View v =  inflater.inflate(R.layout.replace_notification_recycler, container, false);
         notificationRecycler = v.findViewById(R.id.replace_notification_recycler1);
         notificationRecycler.setLayoutManager(new LinearLayoutManager(v.getContext()));
-        notificationRecycler.setAdapter(new NotificationAdapter(v.getContext(), notificationList.getNotifications()));
-
+        notificationRecycler.setAdapter(new NotificationAdapter(v.getContext(), notificationList.getNotifications(),this));
         return v;
     }
 
+    @Override
+    public void onItemClicked(Notification notification) {
+        Toast.makeText(getContext(), notification.getMessage(), Toast.LENGTH_SHORT).show();
+    }
 }
 
