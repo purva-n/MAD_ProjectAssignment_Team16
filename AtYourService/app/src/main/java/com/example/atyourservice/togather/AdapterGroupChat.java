9a5c2118052870c05b19ff74beb0bfcec497870a
divyajangid2496 <92000178+divyajangid2496@users.noreply.github.com>
2022-12-11 11:57:48 -0800

    Divya new (#86)
    * Search changes
    
    * No groups found page
    
    * Ui changes
    
    * back button work for home section
    
    * card view background color change
    
    * Group info changes 1
    
    * group info page changes 2
    
    * Location changes
    
    * Search location changes for create group
    
    * changes
    
    * explore changes
    
    * chat activity change

--- AtYourService/app/src/main/java/com/example/atyourservice/togather/AdapterGroupChat.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/togather/AdapterGroupChat.java
@@ -36,7 +36,7 @@ public class AdapterGroupChat extends RecyclerView.Adapter<HolderGroupChat> {
 
     private Context context;
     private List<Chat> chatList;
-    
+
     public AdapterGroupChat(Context context, List<Chat> chatList){
         this.context = context;
         this.chatList = chatList;
