f8fd78b1240d848c1065b57232c46a510d8db8e4
Purva Nartam <purva.8998@gmail.com>
2022-12-11 20:09:51 -0800

    Fixing rest of the stuff

--- AtYourService/app/src/main/java/com/example/atyourservice/togather/GroupResultsMatchedPageAdapter.java
+++ AtYourService/app/src/main/java/com/example/atyourservice/togather/GroupResultsMatchedPageAdapter.java
@@ -1,6 +1,12 @@
 package com.example.atyourservice.togather;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,31 +14,30 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.atyourservice.R;
 import com.example.atyourservice.models.Group;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.List;
 
-public class GroupResultsMatchedPageAdapter extends RecyclerView.Adapter<GroupResultMatchedPageViewHolder>{
+public class GroupResultsMatchedPageAdapter extends RecyclerView.Adapter<GroupResultMatchedPageViewHolder> {
     private List<Group> groups;
     private Context context;
     private DatabaseReference dbRef;
-    private GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 100;
     private String GpersonEmail;
 
     public GroupResultsMatchedPageAdapter(List<Group> groups, Context context) {
@@ -98,8 +103,6 @@ public class GroupResultsMatchedPageAdapter extends RecyclerView.Adapter<GroupRe
             }
         });
 
-        //TODO: get username
-
         dbRef.child("users").child(GpersonEmail).child("groups").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -121,28 +124,56 @@ public class GroupResultsMatchedPageAdapter extends RecyclerView.Adapter<GroupRe
 
 
         holder.joinGroup.setOnClickListener(view -> {
-//          //TODO: get username
             dbRef.child("users").child(GpersonEmail).child("groups").push().setValue(grp.getId());
             dbRef.child("groups").child(grp.getId()).child("users").push().setValue(GpersonEmail);
-            dbRef.child("groups").child(grp.getId()).child("memberCount").addListenerForSingleValueEvent(new ValueEventListener() {
-                Long memberCount = 0L;
 
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    memberCount = (Long) snapshot.getValue();
-                    assert memberCount != null;
-                    if(dbRef.child("groups").child(grp.getId()).child("memberCount").setValue(memberCount + 1).isSuccessful()) {
-                        Toast.makeText(view.getContext(), "Joined Group", Toast.LENGTH_SHORT).show();
-                    }
-                }
+            // update member count
+            dbRef.child("groups").child(grp.getId()).child("memberCount")
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        Long memberCount = 0L;
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            memberCount = (Long) snapshot.getValue();
+                            dbRef.child("groups").child(grp.getId()).child("memberCount").setValue(memberCount + 1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    holder.getMemberCount().setText(String.valueOf(grp.getMemberCount() + 1));
+                                    Toast.makeText(view.getContext(), "Joined Group", Toast.LENGTH_SHORT).show();
+                                }
+                            });
 
-                }
-            });
+                            //update socialite score for every user in that group
+                            if(grp.getUsers() != null && grp.getUsers().values().size() > 0) {
+                                for (String userid : grp.getUsers().values()) {
+                                    dbRef.child("users").child(userid).child("socialitescore")
+                                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                                Long socialitescore = 0L;
 
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    socialitescore = (Long) snapshot.getValue();
+                                                    dbRef.child("users").child(userid).child("socialitescore")
+                                                            .setValue(socialitescore + 1);
+                                                }
 
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                                }
+                                            });
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+            holder.getJoinGroup().setText("Joined");
+            holder.getJoinGroup().setEnabled(false);
         });
     }
 
@@ -150,4 +181,5 @@ public class GroupResultsMatchedPageAdapter extends RecyclerView.Adapter<GroupRe
     public int getItemCount() {
         return groups.size();
     }
+
 }
